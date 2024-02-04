a0166ccaafcc962dd3067a2891470b054b92189c
KyleD16 <147608077+KyleD16@users.noreply.github.com>
2024-02-04 18:49:42 +0100

    Update BackupDevices.py

--- Code/BackupDevices.py
+++ Code/BackupDevices.py
@@ -9,15 +9,14 @@ def get_router_credentials():
         conn = sqlite3.connect('/home/kyle/routers.db')
         cursor = conn.cursor()
         
-        # Select username, password, and IP from routers table
+     
         cursor.execute("SELECT ip, username, password FROM routers")
         router_credentials = cursor.fetchall()
-        
-        # Select backup time from BackupSchedule table
+      
         cursor.execute("SELECT backup_time FROM BackupSchedule")
         backup_times = cursor.fetchall()
         
-        # Combine the results
+     
         routers = [(ip, username, password, backup_time[0]) 
                    for ip, username, password in router_credentials 
                    for backup_time in backup_times]
@@ -30,7 +29,7 @@ def get_router_credentials():
 
 def perform_backup(ip, username, password):
     try:
-        # Create a dictionary with device information for netmiko
+       
         device = {
             'device_type': 'cisco_ios',
             'ip': ip,
@@ -38,22 +37,22 @@ def perform_backup(ip, username, password):
             'password': password,
         }
 
-        # Connect to the device
+     
         with ConnectHandler(**device) as net_connect:
-            # Send command to retrieve running configuration
+           
             running_config = net_connect.send_command("show running-config")
 
-            # Define the path to store the configuration file
+          
             config_filename = f"{ip}.config"
             local_filepath = f"./{config_filename}"
 
-            # Write running configuration to a local file
+          
             with open(local_filepath, "w") as f:
                 f.write(running_config)
 
             print(f"Backup completed for {ip}. Configuration saved to {local_filepath}")
 
-            # Upload configuration to GitHub
+          
             upload_to_github(local_filepath, ip)
 
     except Exception as e:
@@ -61,22 +60,22 @@ def perform_backup(ip, username, password):
 
 def upload_to_github(local_filepath, router_ip):
     try:
-        # GitHub authentication
+     
         github_token = "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"
         g = Github(github_token)
         
-        # Specify repository details
+       
         repo_owner = "KyleD16"
         repo_name = "Scripting"
         
-        # Get repository
+       
         repo = g.get_user(repo_owner).get_repo(repo_name)
 
-        # Read file content
+      
         with open(local_filepath, "r") as file:
             file_content = file.read()
 
-        # Upload file to repository
+       
         repo.create_file(f"router_configs/{router_ip}_config.txt", f"Backup for {router_ip}", file_content, branch="main")
 
         print(f"Configuration for {router_ip} uploaded to GitHub.")
@@ -86,10 +85,10 @@ def upload_to_github(local_filepath, router_ip):
 def main():
     try:
         while True:
-            # Show current time
+          
             current_time = datetime.datetime.now().time().strftime('%H:%M')
 
-            # Retrieve routers data from the database
+            
             routers = get_router_credentials()
 
             for router in routers:
@@ -97,7 +96,6 @@ def main():
                 if current_time == backup_time:
                     perform_backup(ip, username, password)
 
-            # Sleep for 60 seconds
             time.sleep(60)
 
     except sqlite3.Error as e:
